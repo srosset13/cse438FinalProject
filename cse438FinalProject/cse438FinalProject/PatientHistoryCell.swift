472c9bee9ac4b954d60d5e4b4fd410965c90ccfb
Marissa <marissa.friedman@wustl.edu>
2020-11-20 17:43:34 -0600

    history storyboard draft complete

---
+++ cse438FinalProject/cse438FinalProject/PatientHistoryCell.swift
@@ -0,0 +1,23 @@
+//
+//  PatientHistoryCell.swift
+//  cse438FinalProject
+//
+//  Created by Marissa Friedman on 11/20/20.
+//  Copyright © 2020 Marissa Friedman. All rights reserved.
+//
+
+import UIKit
+
+class PatientHistoryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dateLabel: UIButton!
+    
+    @IBOutlet weak var cognitiveScore: UILabel!
+    @IBOutlet weak var receptiveScore: UILabel!
+    @IBOutlet weak var expressiveScore: UILabel!
+    @IBOutlet weak var fineMotorScore: UILabel!
+    @IBOutlet weak var grossMotorScore: UILabel!
+    
+
+}
+
