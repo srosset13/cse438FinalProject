b4cf5bffd09b303da419e432ba30513117fb441e
Marissa <marissa.friedman@wustl.edu>
2020-12-10 15:47:33 -0800

    updated

--- cse438FinalProject/cse438FinalProject/PatientInfoViewController.swift
+++ cse438FinalProject/cse438FinalProject/PatientInfoViewController.swift
@@ -10,12 +10,18 @@ import Foundation
 import UIKit
 import FirebaseFirestore
 
-class PatientInfoViewController: UIViewController{
+class PatientInfoViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+   
+    
     
     
     @IBOutlet weak var patientName: UILabel!
     @IBOutlet weak var age: UILabel!
     @IBOutlet weak var insuranceProvider: UILabel!
+    @IBOutlet weak var initialBackground: UIView!
+    @IBOutlet weak var initials: UILabel!
+    
+    @IBOutlet weak var patientHistoryCV: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +33,23 @@ class PatientInfoViewController: UIViewController{
         let name = UserDefaults.standard.string(forKey: "username")
         let docID = UserDefaults.standard.string(forKey: "docID")
         print(userID)
+        
+        initialBackground.layer.cornerRadius = 120;
+
+        var initialsText = ""
+                
+        initialsText = String(name?.first ?? " ")
+        
+        print(initialsText)
+        
+        if(name?.contains(" ") == true){
+            var lastName = name?.components(separatedBy: " ").last
+            initialsText = initialsText + String(lastName?.first ?? " ")
+            
+            print(initialsText)
+        }
+        
+        initials.text = initialsText.uppercased()
         
         //set labels to user info
         patientName.text = name
@@ -61,5 +84,32 @@ class PatientInfoViewController: UIViewController{
         
     }
     
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return 1
+       }
+           
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "patientHistoryCell", for: indexPath)
 
+// set testDate label to date of historical tests for each cell
+        
+        
+        return cell
+        
+       }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let detailView = storyboard.instantiateViewController(withIdentifier: "testResults") as! TestResultsViewController
+        
+        // TODO update title on next page
+        // TODO get what test is clicked on and use that from questions dict
+        
+//        detailView.mainViewController = self
+        self.navigationController?.pushViewController(detailView, animated: true)
+    }
+    
 }
---
+++ cse438FinalProject/cse438FinalProject/TestResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TestResultsViewController.swift
+//  cse438FinalProject
+//
+//  Created by Marissa Friedman on 12/10/20.
+//  Copyright © 2020 Marissa Friedman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TestResultsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
